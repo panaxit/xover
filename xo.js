6cb47caf937a4451f16a92b700dc6020ade3627f
Uriel Gomez <uriel@panax.io>
2022-03-03 10:48:59 -0600

    * xover.library.defaults["loading.xslt"] now:   - looks for favicon on the assets folder

--- xo.js
+++ xo.js
@@ -3460,7 +3460,7 @@ xover.library.defaults["loading.xslt"] = xover.xml.createDocument(`
             <div id="no-freeze-spinner">
               <div>
                 <i>
-                  <img src="./custom/images/favicon.png" class="ring_image"/>
+                  <img src="./assets/favicon.png" class="ring_image"/>
                 </i>
                 <div>
                 </div>
@@ -3496,7 +3496,6 @@ xover.library.defaults["message.xslt"] = xover.xml.createDocument(`
             <div class="modal-header alert">
               <h2 class="modal-title font-weight-bold mt-2" style="margin-left: 4rem !important;">¡Aviso!</h2>
               <button type="button" class="close" data-dismiss="modal" aria-label="Close" onclick="this.closest('[role=\\'alertdialog\\']').remove(); ">
-                <!--<img class="gwt-Image mt-2 mr-2" src="./custom/images/circle-x.svg" width="30" height="30"/>-->
                 <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-x-circle text-primary_messages" viewBox="0 0 24 24">
                   <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                   <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
