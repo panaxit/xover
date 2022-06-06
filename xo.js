1d4186b227c861d7f83d2de36adb27f80b2b2d09
Uriel Gómez <uriel@panax.io>
2022-06-06 13:28:52 -0400

    * xover.session.logout is now writable * xover.dom.toExcel refactored for larger objects

--- xo.js
+++ xo.js
@@ -974,7 +974,7 @@ Object.defineProperty(xover.session, 'logout', {
             return false;
         }
     }
-    , writable: false, enumerable: false, configurable: false
+    , writable: true, enumerable: false, configurable: true
 });
 
 Object.defineProperty(xover.session, 'use', {
@@ -7570,17 +7570,19 @@ xover.modernize = function (targetWindow) {
 
 xover.modernize();
 
-xover.dom.toExcel = (function () {
-    //from https://stackoverflow.com/questions/17142427/javascript-to-export-html-table-to-excel
-    var uri = 'data:application/vnd.ms-excel;base64,'
-        , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--><meta http-equiv="content-type" content="text/plain; charset=UTF-8"/></head><body><table>{table}</table></body></html>'
-        , base64 = function (s) { return window.btoa(unescape(encodeURIComponent(s))) }
-        , format = function (s, c) { return s.replace(/{(\w+)}/g, function (m, p) { return c[p]; }) }
-    return function (table, name) {
-        if (!table.nodeType) table = document.getElementById(table)
-        table = table.cloneNode(true);
-        [...table.querySelectorAll('.non_printable')].forEach(el => el.remove())
-        var ctx = { worksheet: name || 'Worksheet', table: table.innerHTML }
-        window.location.href = uri + base64(format(template, ctx))
-    }
-})();
+xover.dom.toExcel = (function (table, name) {
+    if (!table.nodeType) table = document.getElementById(table)
+    table = table.cloneNode(true);
+    [...table.querySelectorAll('.non_printable,input,select')].forEach(el => el.remove());
+    // from https://stackoverflow.com/questions/60483757/aboutblankblocked-error-when-export-table-in-excel
+    var myBlob = new Blob([table.outerHTML], { type: 'application/vnd.ms-excel' });
+    var url = window.URL.createObjectURL(myBlob);
+    var a = document.createElement("a");
+    document.body.appendChild(a);
+    a.href = url;
+    a.download = name;
+    a.click();
+    //adding some delay in removing the dynamically created link solved the problem in FireFox
+    setTimeout(function () { window.URL.revokeObjectURL(url); }, 0);
+
+});
