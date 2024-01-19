944b37e357de29e2bbe5ab5db6b7d4aef1e96aa3
Uriel Gomez <uriel@panax.io>
2024-01-18 23:02:51 -0600

    * xover.json.toXML - bugs fixed

--- xo.js
+++ xo.js
@@ -6506,12 +6506,116 @@ xover.json.toXML = function (json) {
                                 , ']', '</l>')
                             , '{', '<o>')
                         , '}', '</o>')
-                    , '\\s', '<s/>', 1)
-                , '"([^"]+?)"\\:', '<a>$1</a>', 1)
+                    , '"([^"]+?)"\\:\\s*', '<a>$1</a>', 1)
+                , '\\s', '<s/>', 1)
             , '<l>([^<]+)</l>', '<l>$1</l>', 1)
     );
 
-    let reformated_xson = raw_xson.transform(xover.xml.createDocument(`<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns="" version="1.0" id="raw_json_compatibility"><xsl:variable name="node_name">olsc</xsl:variable><xsl:variable name="translate-o">{[ ,</xsl:variable><xsl:variable name="translate-c">}] </xsl:variable><xsl:template match="/"><xsl:apply-templates></xsl:apply-templates></xsl:template><xsl:template match="*" mode="value"><xsl:copy><xsl:copy-of select="@*"></xsl:copy-of><xsl:apply-templates></xsl:apply-templates></xsl:copy></xsl:template><xsl:template match="o|l|c" mode="value"><xsl:param name="is_string" select="false()"></xsl:param><xsl:value-of select="translate(name(),$node_name,$translate-o)"/><xsl:apply-templates select="(text()|*)[1]" mode="value"><xsl:with-param name="is_string" select="$is_string"></xsl:with-param></xsl:apply-templates><xsl:value-of select="translate(name(),$node_name,$translate-c)"/><xsl:apply-templates select="(following-sibling::text()|following-sibling::*)[1]" mode="value"><xsl:with-param name="is_string" select="$is_string"></xsl:with-param></xsl:apply-templates></xsl:template><xsl:template match="s" mode="value"><xsl:param name="is_string" select="false()"></xsl:param><xsl:value-of select="' '"/><xsl:if test="$is_string"><xsl:apply-templates select="(following-sibling::text()|following-sibling::*)[1]" mode="value"><xsl:with-param name="is_string" select="$is_string"></xsl:with-param></xsl:apply-templates></xsl:if></xsl:template><xsl:template match="r|f" mode="value"><xsl:param name="is_string" select="false()"></xsl:param><xsl:text></xsl:text><xsl:apply-templates select="(text()|*)[1]" mode="value"><xsl:with-param name="is_string" select="$is_string"></xsl:with-param></xsl:apply-templates><xsl:apply-templates select="(following-sibling::text()|following-sibling::*)[1]" mode="value"><xsl:with-param name="is_string" select="$is_string"></xsl:with-param></xsl:apply-templates></xsl:template><xsl:template match="e" mode="value"><xsl:param name="is_string" select="false()"></xsl:param><xsl:text>\</xsl:text><xsl:value-of select="text()"/><xsl:apply-templates select="(following-sibling::text()|following-sibling::*)[1]" mode="value"><xsl:with-param name="is_string" select="$is_string"></xsl:with-param></xsl:apply-templates></xsl:template><xsl:template match="text()" mode="value"><xsl:param name="is_string" select="false()"></xsl:param><xsl:copy></xsl:copy><xsl:if test="$is_string and not(substring(.,string-length(.),1)='&quot;')"><xsl:apply-templates select="(following-sibling::text()|following-sibling::*)[1]" mode="value"><xsl:with-param name="is_string" select="$is_string"></xsl:with-param></xsl:apply-templates></xsl:if></xsl:template><xsl:template match="text()[substring(.,1,1)='&quot;']" mode="value"><xsl:param name="is_string" select="false()"></xsl:param><xsl:copy></xsl:copy><xsl:if test="not(substring(.,string-length(.),1)='&quot;')"><xsl:apply-templates select="(following-sibling::text()|following-sibling::*)[1]" mode="value"><xsl:with-param name="is_string" select="true()"></xsl:with-param></xsl:apply-templates></xsl:if></xsl:template><xsl:template match="l/text()"><xsl:element name="v"><xsl:value-of select="."/></xsl:element></xsl:template><xsl:template match="l"><xsl:copy><xsl:copy-of select="@*"></xsl:copy-of><xsl:apply-templates select="o|text()"></xsl:apply-templates></xsl:copy></xsl:template><xsl:template match="o"><xsl:copy><xsl:copy-of select="@*"></xsl:copy-of><xsl:apply-templates select="a"></xsl:apply-templates></xsl:copy></xsl:template><xsl:template match="a"><xsl:variable name="following" select="(following-sibling::text()|following-sibling::*[not(self::f or self::r or self::c or self::s)])[1]"></xsl:variable><xsl:copy><xsl:element name="n"><xsl:value-of select="text()"/></xsl:element><xsl:choose><xsl:when test="$following/self::o or $following/self::l"><xsl:apply-templates select="$following"></xsl:apply-templates></xsl:when><xsl:otherwise><xsl:element name="v"><xsl:apply-templates select="$following" mode="value"></xsl:apply-templates></xsl:element></xsl:otherwise></xsl:choose></xsl:copy></xsl:template></xsl:stylesheet>`));
+    let reformated_xson = raw_xson.transform(xover.xml.createDocument(`<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns="" version="1.0" id="raw_json_compatibility">
+	<xsl:variable name="node_name">olsc</xsl:variable>
+	<xsl:variable name="translate-o">{[ ,</xsl:variable>
+	<xsl:variable name="translate-c">}] </xsl:variable>
+	<xsl:template match="/">
+		<xsl:apply-templates/>
+	</xsl:template>
+	<xsl:template match="*" mode="value">
+		<xsl:copy>
+			<xsl:copy-of select="@*"/>
+			<xsl:apply-templates/>
+		</xsl:copy>
+	</xsl:template>
+	<xsl:template match="o|l|c" mode="value">
+		<xsl:param name="is_string" select="false()"/>
+		<xsl:value-of select="translate(name(),$node_name,$translate-o)"/>
+		<xsl:apply-templates select="(text()|*)[1]" mode="value">
+			<xsl:with-param name="is_string" select="$is_string"/>
+		</xsl:apply-templates>
+		<xsl:value-of select="translate(name(),$node_name,$translate-c)"/>
+		<xsl:apply-templates select="(following-sibling::text()|following-sibling::*)[1]" mode="value">
+			<xsl:with-param name="is_string" select="$is_string"/>
+		</xsl:apply-templates>
+	</xsl:template>
+	<xsl:template match="s" mode="value">
+		<xsl:param name="is_string" select="false()"/>
+		<xsl:value-of select="' '"/>
+		<xsl:if test="$is_string">
+			<xsl:apply-templates select="(following-sibling::text()|following-sibling::*)[1]" mode="value">
+				<xsl:with-param name="is_string" select="$is_string"/>
+			</xsl:apply-templates>
+		</xsl:if>
+	</xsl:template>
+	<xsl:template match="r|f" mode="value">
+		<xsl:param name="is_string" select="false()"/>
+		<xsl:text>\n</xsl:text>
+		<xsl:apply-templates select="(text()|*)[1]" mode="value">
+			<xsl:with-param name="is_string" select="$is_string"/>
+		</xsl:apply-templates>
+		<xsl:apply-templates select="(following-sibling::text()|following-sibling::*)[1]" mode="value">
+			<xsl:with-param name="is_string" select="$is_string"/>
+		</xsl:apply-templates>
+	</xsl:template>
+	<xsl:template match="e" mode="value">
+		<xsl:param name="is_string" select="false()"/>
+		<xsl:text>\\</xsl:text>
+		<xsl:value-of select="text()"/>
+		<xsl:apply-templates select="(following-sibling::text()|following-sibling::*)[1]" mode="value">
+			<xsl:with-param name="is_string" select="$is_string"/>
+		</xsl:apply-templates>
+	</xsl:template>
+	<xsl:template match="text()" mode="value">
+		<xsl:param name="is_string" select="false()"/>
+		<xsl:copy/>
+		<xsl:if test="$is_string and not(substring(.,string-length(.),1)='&quot;')">
+			<xsl:apply-templates select="(following-sibling::text()|following-sibling::*)[1]" mode="value">
+				<xsl:with-param name="is_string" select="$is_string"/>
+			</xsl:apply-templates>
+		</xsl:if>
+	</xsl:template>
+	<xsl:template match="text()[substring(.,1,1)='&quot;']" mode="value">
+		<xsl:param name="is_string" select="false()"/>
+		<xsl:copy/>
+		<xsl:if test="$is_string = false() and (string-length(.)=1 or not(substring(.,string-length(.),1)='&quot;'))">
+			<xsl:apply-templates select="(following-sibling::text()|following-sibling::*)[1]" mode="value">
+				<xsl:with-param name="is_string" select="true()"/>
+			</xsl:apply-templates>
+		</xsl:if>
+	</xsl:template>
+	<xsl:template match="l/text()">
+		<xsl:element name="v">
+			<xsl:apply-templates mode="value" select="."/>
+		</xsl:element>
+	</xsl:template>
+	<xsl:template match="l">
+		<xsl:copy>
+			<xsl:copy-of select="@*"/>
+			<xsl:apply-templates select="o|text()[normalize-space(.)!='']|c"/>
+		</xsl:copy>
+	</xsl:template>
+	<xsl:template match="o">
+		<xsl:copy>
+			<xsl:copy-of select="@*"/>
+			<xsl:apply-templates select="a"/>
+		</xsl:copy>
+	</xsl:template>
+	<xsl:template match="a">
+		<xsl:variable name="following" select="(following-sibling::text()[normalize-space(.)!='']|following-sibling::*[not(self::f or self::r or self::c or self::s)])[1]"/>
+		<xsl:copy>
+			<xsl:element name="n">
+				<xsl:value-of select="text()"/>
+			</xsl:element>
+			<xsl:choose>
+				<xsl:when test="$following/self::o or $following/self::l">
+					<xsl:apply-templates select="$following"/>
+				</xsl:when>
+				<xsl:otherwise>
+					<xsl:element name="v">
+						<xsl:apply-templates select="$following" mode="value"/>
+					</xsl:element>
+				</xsl:otherwise>
+			</xsl:choose>
+		</xsl:copy>
+	</xsl:template>
+</xsl:stylesheet>`));
 
     let xson = reformated_xson.transform(xover.xml.createDocument(`<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xson="http://panax.io/xson" xmlns="" version="1.0" id="PrettifyJSON"><xsl:variable name="validChars" select="'abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ0123456789-_'"/><xsl:template match="/"><xsl:apply-templates mode="raw-to-xson"/></xsl:template><xsl:template match="*" mode="raw-to-xson"><xsl:apply-templates mode="raw-to-xson"/></xsl:template><xsl:template match="o|l" mode="raw-to-xson"><xsl:apply-templates mode="raw-to-xson"/></xsl:template><xsl:template match="l/v" mode="raw-to-xson"><xsl:element name="xson:item"><xsl:apply-templates mode="raw-to-xson"/></xsl:element></xsl:template><xsl:template match="a" mode="raw-to-xson"><xsl:variable name="name"><xsl:choose><xsl:when test="number(translate(n,'&quot;',''))=translate(n,'&quot;','')"><xsl:value-of select="concat('@',translate(n,'&quot;',''))"/></xsl:when><xsl:otherwise><xsl:value-of select="translate(translate(n,'&quot;',''),translate(n,$validChars,''),'@@@@@@@@@@@@@@@')"/></xsl:otherwise></xsl:choose></xsl:variable><xsl:element name="{translate($name,'@','_')}"><xsl:if test="contains($name,'@')"><xsl:attribute name="xson:originalName"><xsl:value-of select="translate(n,'&quot;','')"/></xsl:attribute></xsl:if><xsl:if test="l"><xsl:attribute name="xsi:type">xson:array</xsl:attribute></xsl:if><xsl:apply-templates select="*" mode="raw-to-xson"/></xsl:element></xsl:template><xsl:template match="text()" mode="raw-to-xson"><xsl:value-of select="."/></xsl:template><xsl:template match="text()[starts-with(.,'&quot;')]" mode="raw-to-xson"><xsl:value-of select="substring(.,2,string-length(.)-2)"/></xsl:template><xsl:template match="text()[.='null']|*[.='']" mode="raw-to-xson"/><xsl:template match="text()[.='null']" mode="raw-to-xson"><xsl:attribute name="xsi:nil">true</xsl:attribute></xsl:template><xsl:template match="n" mode="raw-to-xson"/><xsl:template match="a[v='true' or v='false']/n" mode="raw-to-xson"><xsl:attribute name="xsi:type">boolean</xsl:attribute></xsl:template><xsl:template match="e" mode="raw-to-xson"><xsl:value-of select="@v"/></xsl:template><xsl:template match="a[number(v)=v]/n" mode="raw-to-xson"><xsl:attribute name="xsi:type">numeric</xsl:attribute></xsl:template><xsl:template match="a[starts-with(v,'&quot;')]/n" mode="raw-to-xson"><xsl:attribute name="xsi:type">string</xsl:attribute></xsl:template><xsl:template match="a[l]/n" mode="raw-to-xson"><xsl:attribute name="xsi:type">xson:array</xsl:attribute></xsl:template><xsl:template match="a[o]/n" mode="raw-to-xson"><xsl:attribute name="xsi:type">xson:object</xsl:attribute></xsl:template><xsl:template match="o[not(preceding-sibling::n)]" mode="raw-to-xson"><xsl:element name="xson:object"><xsl:apply-templates mode="raw-to-xson"/></xsl:element></xsl:template><xsl:template match="l[not(preceding-sibling::n)]" mode="raw-to-xson"><xsl:element name="xson:array"><xsl:apply-templates mode="raw-to-xson"/></xsl:element></xsl:template></xsl:stylesheet>`));
 
