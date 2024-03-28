7200d3ef8f4bcbeb13c525c6bfe56c3434a28c07
Uriel Gomez <uriel@panax.io>
2024-03-27 20:11:50 -0600

    * xover.sources.defaults["loading.xslt"] - simplified template to a nempty div

--- xo.js
+++ xo.js
@@ -9759,23 +9759,7 @@ xover.sources.defaults["loading.xslt"] = xover.xml.createDocument(`
     <xsl:output method="xml" indent="no" />
     <xsl:param name="js:icon"><![CDATA[[...document.querySelectorAll('link[type = "image/x-icon"]')].map(el => el && el.getAttribute("href"))[0]]]></xsl:param>
     <xsl:template match="node()">
-    <div class="loading" onclick="this.remove()" role="alert" aria-busy="true">
-      <div class="modal_content-loading">
-        <div class="modal-dialog modal-dialog-centered">
-          <div class="no-freeze-spinner">
-            <div id="no-freeze-spinner">
-              <div>
-                <i class="icon" style="justify-content: center; display: flex; align-items: center;">
-                    <img src="{$js:icon}" class="ring_image" onerror="this.remove()"/><span class="details" style="position: absolute; top: 3rem; width: 100%;"><progress style="display:none; width: 100%; accent-color: var(--progress-color, green);" max="100" value="0" aria-label="Loading…">0%</progress></span>
-                </i>
-                <div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <div class="loading xo-loading" onclick="this.remove()" role="alert" aria-busy="true"/>
     </xsl:template>                                                                                     
     <xsl:template match="text()|processing-instruction()|comment()"/>
 </xsl:stylesheet>`);
