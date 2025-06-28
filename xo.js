4a189a0068cd801f8e59dfac84247cb0d942797f
Uriel Gomez <uriel@panax.io>
2025-06-28 08:34:21 -0600

    * xover.Manifest - fixed state datatype * xover.xml.getDifferences - bug fixxed

--- xo.js
+++ xo.js
@@ -2350,7 +2350,7 @@ xover.Manifest = function (manifest = {}) {
         "site": {},
         "sources": {},
         "start": [],
-        "state": [],
+        "state": {},
         "spaces": {},
         "settings": {}
     }
@@ -3556,7 +3556,7 @@ xover.xml.getDifferences = function (node1, node2, composed = false) {
     if (this === xover.xml && node1.nodeType === Node.ELEMENT_NODE && node1.hasAttribute("xo-stylesheet")) {
         return all_differences;
     }
-    if (node1.attributes["xo-scope"] != node2.attributes["xo-scope"] || node1.hasOwnProperty("scope") && node2.hasOwnProperty("scope") && node1.scope !== node2.scope) {
+    if ((node1.attributes || {})["xo-scope"] != (node2.attributes || {})["xo-scope"] || node1.hasOwnProperty("scope") && node2.hasOwnProperty("scope") && node1.scope !== node2.scope) {
         all_differences.push(new Map([[node1, node2]]));
         return all_differences;
     }
