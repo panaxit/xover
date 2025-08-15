4292b221da4fa6ddde211aed16e62b89807efa19
Uriel Gomez <uriel@panax.io>
2025-08-14 21:11:02 -0600

    * xover.json.toXML - fixed array transformation

--- xo.js
+++ xo.js
@@ -14592,7 +14592,7 @@ xover.json.toXML = function (json) {
 			</xsl:apply-templates>
 		</xsl:if>
 	</xsl:template>
-	<xsl:template match="text()[substring(.,1,1)='&quot;']" mode="value">
+	<xsl:template match="text()[substring(normalize-space(.),1,1)='&quot;']" mode="value">
 		<xsl:param name="is_string" select="false()"/>
 		<xsl:copy/>
 		<xsl:if test="$is_string = false() and (string-length(.)=1 or not(substring(.,string-length(.),1)='&quot;'))">
@@ -14609,7 +14609,7 @@ xover.json.toXML = function (json) {
 	<xsl:template match="l">
 		<xsl:copy>
 			<xsl:copy-of select="@*"/>
-			<xsl:apply-templates select="o|text()[normalize-space(.)!='']|c"/>
+			<xsl:apply-templates select="*[not(self::c)]|text()[normalize-space(.)!='']"/>
 		</xsl:copy>
 	</xsl:template>
 	<xsl:template match="o">
@@ -14622,7 +14622,9 @@ xover.json.toXML = function (json) {
 		<xsl:variable name="following" select="(following-sibling::text()[normalize-space(.)!='']|following-sibling::*[not(self::f or self::r or self::c or self::s)])[1]"/>
 		<xsl:copy>
 			<xsl:element name="n">
-				<xsl:value-of select="text()"/>
+				<xsl:apply-templates select="(text()|*)[1]" mode="value">
+					<xsl:with-param name="is_string" select="true()"/>
+				</xsl:apply-templates>
 			</xsl:element>
 			<xsl:choose>
 				<xsl:when test="$following/self::o or $following/self::l">
@@ -14638,7 +14640,79 @@ xover.json.toXML = function (json) {
 	</xsl:template>
 </xsl:stylesheet>`));
 
-    let xson = reformated_xson.transform(xover.xml.createDocument(`<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xson="http://panax.io/xson" xmlns="" version="1.0" id="PrettifyJSON"><xsl:variable name="validChars" select="'abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ0123456789-_'"/><xsl:template match="/"><xsl:apply-templates mode="raw-to-xson"/></xsl:template><xsl:template match="*" mode="raw-to-xson"><xsl:apply-templates mode="raw-to-xson"/></xsl:template><xsl:template match="o|l" mode="raw-to-xson"><xsl:apply-templates mode="raw-to-xson"/></xsl:template><xsl:template match="l/v" mode="raw-to-xson"><xsl:element name="xson:item"><xsl:apply-templates mode="raw-to-xson"/></xsl:element></xsl:template><xsl:template match="a" mode="raw-to-xson"><xsl:variable name="name"><xsl:choose><xsl:when test="number(translate(n,'&quot;',''))=translate(n,'&quot;','')"><xsl:value-of select="concat('@',translate(n,'&quot;',''))"/></xsl:when><xsl:otherwise><xsl:value-of select="translate(translate(n,'&quot;',''),translate(n,$validChars,''),'@@@@@@@@@@@@@@@')"/></xsl:otherwise></xsl:choose></xsl:variable><xsl:element name="{translate($name,'@','_')}"><xsl:if test="contains($name,'@')"><xsl:attribute name="xson:originalName"><xsl:value-of select="translate(n,'&quot;','')"/></xsl:attribute></xsl:if><xsl:if test="l"><xsl:attribute name="xsi:type">xson:array</xsl:attribute></xsl:if><xsl:apply-templates select="*" mode="raw-to-xson"/></xsl:element></xsl:template><xsl:template match="text()" mode="raw-to-xson"><xsl:value-of select="."/></xsl:template><xsl:template match="text()[starts-with(.,'&quot;')]" mode="raw-to-xson"><xsl:value-of select="substring(.,2,string-length(.)-2)"/></xsl:template><xsl:template match="text()[.='null']|*[.='']" mode="raw-to-xson"/><xsl:template match="text()[.='null']" mode="raw-to-xson"><xsl:attribute name="xsi:nil">true</xsl:attribute></xsl:template><xsl:template match="n" mode="raw-to-xson"/><xsl:template match="a[v='true' or v='false']/n" mode="raw-to-xson"><xsl:attribute name="xsi:type">boolean</xsl:attribute></xsl:template><xsl:template match="e" mode="raw-to-xson"><xsl:value-of select="@v"/></xsl:template><xsl:template match="a[number(v)=v]/n" mode="raw-to-xson"><xsl:attribute name="xsi:type">numeric</xsl:attribute></xsl:template><xsl:template match="a[starts-with(v,'&quot;')]/n" mode="raw-to-xson"><xsl:attribute name="xsi:type">string</xsl:attribute></xsl:template><xsl:template match="a[l]/n" mode="raw-to-xson"><xsl:attribute name="xsi:type">xson:array</xsl:attribute></xsl:template><xsl:template match="a[o]/n" mode="raw-to-xson"><xsl:attribute name="xsi:type">xson:object</xsl:attribute></xsl:template><xsl:template match="o[not(preceding-sibling::n)]" mode="raw-to-xson"><xsl:element name="xson:object"><xsl:apply-templates mode="raw-to-xson"/></xsl:element></xsl:template><xsl:template match="l[not(preceding-sibling::n)]" mode="raw-to-xson"><xsl:element name="xson:array"><xsl:apply-templates mode="raw-to-xson"/></xsl:element></xsl:template></xsl:stylesheet>`));
+    let xson = reformated_xson.transform(xover.xml.createDocument(`<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xson="http://panax.io/xson" xmlns="" version="1.0" id="PrettifyJSON">
+	<xsl:variable name="validChars" select="'abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ0123456789-_.'"/>
+	<xsl:template match="/">
+		<xsl:apply-templates mode="raw-to-xson"/>
+	</xsl:template>
+	<xsl:template match="*" mode="raw-to-xson">
+		<xsl:apply-templates mode="raw-to-xson"/>
+	</xsl:template>
+	<xsl:template match="o|l" mode="raw-to-xson">
+		<xsl:apply-templates mode="raw-to-xson"/>
+	</xsl:template>
+	<xsl:template match="l/v" mode="raw-to-xson">
+		<xsl:element name="xson:item">
+			<xsl:apply-templates mode="raw-to-xson"/>
+		</xsl:element>
+	</xsl:template>
+	<xsl:template match="a" mode="raw-to-xson">
+		<xsl:variable name="name">
+			<xsl:if test="number(translate(substring(n,1,1),'&quot;',''))=translate(substring(n,1,1),'&quot;','')">@</xsl:if>
+			<xsl:value-of select="translate(translate(n,'&quot;',''),translate(n,$validChars,' '),'@@@@@@@@@@@@@@@')"/>
+		</xsl:variable>
+		<xsl:element name="{translate($name,'@','_')}">
+			<xsl:if test="contains($name,'@')">
+				<xsl:attribute name="xson:originalName">
+					<xsl:value-of select="translate(n,'&quot;','')"/>
+				</xsl:attribute>
+			</xsl:if>
+			<xsl:if test="l">
+				<xsl:attribute name="xsi:type">xson:array</xsl:attribute>
+			</xsl:if>
+			<xsl:apply-templates select="*" mode="raw-to-xson"/>
+		</xsl:element>
+	</xsl:template>
+	<xsl:template match="text()" mode="raw-to-xson">
+		<xsl:value-of select="normalize-space(.)"/>
+	</xsl:template>
+	<xsl:template match="text()[starts-with(normalize-space(.),'&quot;')]" mode="raw-to-xson">
+		<xsl:value-of select="substring(normalize-space(.),2,string-length(normalize-space(.))-2)"/>
+	</xsl:template>
+	<xsl:template match="*[.='']" mode="raw-to-xson"/>
+	<xsl:template match="text()[normalize-space(.)='null']" mode="raw-to-xson">
+		<xsl:attribute name="xsi:nil">true</xsl:attribute>
+	</xsl:template>
+	<xsl:template match="n" mode="raw-to-xson"/>
+	<xsl:template match="a[normalize-space(v)='true' or normalize-space(v)='false']/n" mode="raw-to-xson">
+		<xsl:attribute name="xsi:type">boolean</xsl:attribute>
+	</xsl:template>
+	<xsl:template match="e" mode="raw-to-xson">
+		<xsl:value-of select="@v"/>
+	</xsl:template>
+	<xsl:template match="a[number(v)=v]/n" mode="raw-to-xson">
+		<xsl:attribute name="xsi:type">numeric</xsl:attribute>
+	</xsl:template>
+	<xsl:template match="a[starts-with(normalize-space(v),'&quot;')]/n" mode="raw-to-xson">
+		<xsl:attribute name="xsi:type">string</xsl:attribute>
+	</xsl:template>
+	<xsl:template match="a[l]/n" mode="raw-to-xson">
+		<xsl:attribute name="xsi:type">xson:array</xsl:attribute>
+	</xsl:template>
+	<xsl:template match="a[o]/n" mode="raw-to-xson">
+		<xsl:attribute name="xsi:type">xson:object</xsl:attribute>
+	</xsl:template>
+	<xsl:template match="o[not(preceding-sibling::n)]" mode="raw-to-xson">
+		<xsl:element name="xson:object">
+			<xsl:apply-templates mode="raw-to-xson"/>
+		</xsl:element>
+	</xsl:template>
+	<xsl:template match="l[not(preceding-sibling::n)]" mode="raw-to-xson">
+		<xsl:element name="xson:array">
+			<xsl:apply-templates mode="raw-to-xson"/>
+		</xsl:element>
+	</xsl:template>
+</xsl:stylesheet>`));
 
     xson.normalizeNamespaces();
     return xson;
