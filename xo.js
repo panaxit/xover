c352589e66fedccd6d34a3406edf068ed17e1773
Uriel <uriel@panax.io>
2026-01-26 00:55:41 -0600

    * str2blks_MD5 - bug fixed

--- xo.js
+++ xo.js
@@ -85,15 +85,17 @@ xover.cryptography.encodeMD5 = function (str) {
 	 * Append padding bits and the length, as described in the MD5 standard.
 	 */
 	function str2blks_MD5(str) {
-		let i;
-		nblk = ((str.length + 8) >> 6) + 1;
-		blks = new Array(nblk * 16);
-		for (let i = 0; i < nblk * 16; i++) blks[i] = 0;
-		for (let i = 0; i < str.length; i++)
-			blks[i >> 2] |= str.charCodeAt(i) << ((i % 4) * 8);
-		blks[i >> 2] |= 0x80 << ((i % 4) * 8);
-		blks[nblk * 16 - 2] = str.length * 8;
-		return blks;
+		let i = 0
+		let nblk = ((str.length + 8) >> 6) + 1
+		let blks = new Array(nblk * 16)
+
+		for (i = 0; i < nblk * 16; i++) blks[i] = 0
+		for (i = 0; i < str.length; i++)
+			blks[i >> 2] |= str.charCodeAt(i) << ((i % 4) * 8)
+
+		blks[i >> 2] |= 0x80 << ((i % 4) * 8)
+		blks[nblk * 16 - 2] = str.length * 8
+		return blks
 	}
 
 	/*
