9c53e5c4bbf15d559bc974359dd1121f8634db84
Uriel <uriel@panax.io>
2026-06-08 11:30:54 -0600

    * xover.storehouse.read - restores applying original mime-type

--- xo.js
+++ xo.js
@@ -475,7 +475,7 @@ Object.defineProperties(xover.storehouse, {
 				if (record && record.type.indexOf("json") != -1) {
 					document = JSON.parse(content)
 				} else {
-					document = content && await xover.xml.createDocument(content) || content
+					document = content && await xover.xml.createDocument(content, { mime_type: record.type.split(";")[0] }) || content
 				}
 			} catch (e) {
 				console.log(e)
